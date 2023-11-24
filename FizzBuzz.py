872d5a2921695c1f88eb9590f975d1f9cfbf37e7
Mert Yavas <137993034+mert-yavas@users.noreply.github.com>
2023-11-24 22:54:06 +0300

    Add files via upload

---
+++ FizzBuzz.py
@@ -0,0 +1,10 @@
+number = int(input("Enter number for Fizzbuzz: "))
+for i in range(1, number + 1):
+    if i % 15 == 0:
+      print("FizzBuzz")
+    elif i % 5 == 0:
+      print("Buzz")
+    elif i % 3 == 0:
+      print("Fizz")
+    else:
+      print(i)
